4940a562c09a4a10c997f0d098280ead92356354
jakesauter <jake.sauter3@gmail.com>
2020-08-27 10:12:07 -0400

    tracking all files

---
+++ python_practice/first.py
@@ -0,0 +1,17 @@
+# %%
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+
+# %%
+x = np.array([1,2,3])
+
+# %%
+tf.constant(x)
+
+#%%
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3], [1,2,3])
